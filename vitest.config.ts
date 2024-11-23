4e7f31d91233f85546f1a624dce40211876c2cc1
JonathanSaudhof <jonathan.saudhof@gmail.com>
2024-11-23 11:25:13 +0100

    chore: add react testing library

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 /// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+    setupFiles: "./tests/setup.js", // assuming the test folder is in the root of our project
   },
   resolve: {
     alias: {
